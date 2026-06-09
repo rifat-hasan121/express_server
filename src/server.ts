466ddf6a9675acd7422cecef5c493c35b4ad9d6c
Rifat Hasan <rifathasannetrakona@gmail.com>
2026-06-09 22:06:14 +0600

    added post route, get all users, get single users

--- src/server.ts
+++ src/server.ts
@@ -16,35 +16,6 @@ const pool = new Pool({
   connectionString: `${process.env.CONNECTION_STR}`,
 });
 
-// const initDB = async()=>{
-//     await pool.query(`
-//         CREATE TABLE IF NOT EXISTS users (
-//         id SERIAL PRIMARY KEY,
-//         name VARCHAR(100) NOT NULL,
-//         email VARCHAR(150) UNIQUE NOT NULL,
-//         age INT,
-//         phone VARCHAR(15),
-//         address VARCHAR(200),
-//         created_at TIMESTAMP DEFAULT NOW(),
-//         updated_at TIMESTAMP DEFAULT NOW()
-//         )
-//         `)
-
-//         await pool.query(`
-//             CREATE TABLE IF NOT EXISTS todos(
-//             id SERIAL PRIMARY KEY,
-//             user_id INT REFERENCES users(id) ON DELETE CASCADE,
-//             title VARCHAR(200) NOT NULL,
-//             description TEXT,
-//             completed BOOLEAN DEFAULT FALSE,
-//             due_date DATE,
-//             created_at TIMESTAMP DEFAULT NOW(),
-//             updated_at TIMESTAMP DEFAULT NOW()
-//             )
-//             `)
-// }
-
-
 const initDB = async () => {
   try {
     console.log("🔄 Initializing database tables...");
@@ -87,16 +58,71 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-
-app.post('/', (req: Request, res: Response) => {
-  console.log(req.body);
-
-  res.status(200).json({
-    status: 'success',
-    message: 'Post request received successfully',
-    data: req.body,
-  });
+// users CURD
+app.post('/users', async(req: Request, res: Response) => {
+  const {name, email}=req.body;
+  
+  try {
+    const result= await pool.query(`INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *` , [name, email]);
+    console.log(result.rows[0])
+    res.status(201).json({
+      success: false,
+      data: result.rows[0],
+    })
+    
+  } catch (error:any) {
+    res.status(400).json({
+      status: 'error',
+      success:false,
+      message: error.message,
+    })
+  }
 });
+
+app.get('/users', async (req:Request, res:Response)=>{
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        res.status(200).json({
+            success:true,
+            data: result.rows,
+        });
+    } catch (error:any) {
+        res.status(500).json({
+            success:false,
+            message: error.message,
+        })
+    }
+})
+
+app.get('/users/:id', async(req:Request, res:Response)=>{
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+        
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'User not found',
+            })
+        } else{
+               res.status(200).json({
+            success:true,
+            message: 'User fetched successfully',
+            data: result.rows[0],
+        })
+        }
+     
+    } catch (error:any) {
+          res.status(500).json({
+            success:false,
+            message: error.message,
+        })
+    }
+})
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
