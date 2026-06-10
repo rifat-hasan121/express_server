3aa334b97af318b7667f1da59e62ed30e7a93127
Rifat Hasan <rifathasannetrakona@gmail.com>
2026-06-10 23:48:46 +0600

    added curd operation for todos

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-import { Pool } from 'pg';
+import express, { NextFunction, Request, Response } from 'express';
+import { Pool, Result } from 'pg';
 import dotenv from 'dotenv';
 import path from 'path';
 
@@ -54,7 +54,12 @@ const initDB = async () => {
 
 initDB();
 
-app.get('/', (req: Request, res: Response) => {
+const logger = (req: Request, res:Response, next: NextFunction)=>{
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+}
+
+app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
@@ -190,7 +195,103 @@ app.post('/todos', async(req: Request, res: Response)=>{
   }
 })
 
+app.get('/todos', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+})
 
+app.get('/todos/:id', async(req:Request, res:Response)=>{
+  const result= await pool.query(`SELECT * FROM todos WHERE id = $1`, [req.params.id]);
+
+  try {
+    if(result.rows.length===0){
+      res.status(404).json({
+        success: false,
+        message: 'Todo not found',
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: 'Todo fetched successfully',
+        data: result.rows[0],
+      })
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+})
+
+app.put('/todos/:id',async(req:Request, res:Response)=>{
+  const{title}=req.body;
+  const result= await pool.query(`UPDATE todos SET title = $1 WHERE id = $2 RETURNING *`, [title, req.params.id])
+
+  try {
+    if(result.rows.length===0){
+      res.status(404).json({
+        success: false,
+        message: 'Todo not found',
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: 'Todo updated successfully',
+        data: result.rows[0],
+      })
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+})
+
+app.delete('/todos/:id', async(req:Request, res:Response)=>{
+  const result = await pool.query(`DELETE FROM todos WHERE id =$1 RETURNING *`, [req.params.id]);
+
+  try {
+    if(result.rowCount ===0){
+      res.status(404).json({
+        success: false,
+        message: 'Todo not found',
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: 'Todo deleted successfully',
+        data: null,
+      })
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+})
+
+
+// ROUTE NOT FOUND
+
+app.use((req:Request, res:Response)=>{
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
